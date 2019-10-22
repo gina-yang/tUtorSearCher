f9ccb401a4731fef37713112e265abd1e2a461f1
Karalee Mota <karaleem@usc.edu>
2019-10-21 18:54:08 -0700

    created abstract 'User' class

---
+++ app/src/main/java/User.java
@@ -0,0 +1,9 @@
+public abstract class User
+{
+    private String name;
+    private int age;
+    private String gender;//male, female, other
+    private String profilePic;//url to the profile pic
+    private String email;//should be a usc email
+    
+}
