ba9a51e37e7f8676b691d607e1165ea9cc0e9545
Gina Yang <ginayang@usc.edu>
2019-10-28 15:05:55 -0700

    Added dependencies for firebase and removed MySQL from DBAccessor

--- app/src/main/java/DBAccessor.java
+++ app/src/main/java/DBAccessor.java
@@ -1,25 +1,15 @@
-import java.sql.Connection;
-import java.sql.ResultSet;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
 
-public class DBAccessor
-{
+public class DBAccessor {
     private String db_username;
     private String db_password;
     private String db_url;
-    private String jdbcDriverPath;
-    private Connection db_connection;
 
-    /**The constructor will open the connection to the database and store the
-     * connection in the DB_Connection variable
-     * */
-    public DBAccessor()
-    {
+    private FirebaseAuth mAuth;
+    private FirebaseFirestore db;
 
-    }
-    public ResultSet executeQuery(String query)
-    {
-        return null;
-    }
+
 }
 
 
