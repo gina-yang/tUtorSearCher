b18fbcfdb2719483df3116294f5d9f220443f2cf
Karalee Mota <karaleem@usc.edu>
2019-10-30 01:22:43 -0700

    changed startup activity to login

--- app/src/main/java/com/example/tutorsearcher/ui/home/SearchFragment.java
+++ app/src/main/java/com/example/tutorsearcher/ui/home/SearchFragment.java
@@ -12,7 +12,7 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 import com.example.tutorsearcher.R;
 
-public class HomeFragment extends Fragment {
+public class SearchFragment extends Fragment {
 
     private HomeViewModel homeViewModel;
 
@@ -20,12 +20,12 @@ public class HomeFragment extends Fragment {
             ViewGroup container, Bundle savedInstanceState) {
         homeViewModel =
                 ViewModelProviders.of(this).get(HomeViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_home, container, false);
-        final TextView textView = root.findViewById(R.id.text_home);
+        View root = inflater.inflate(R.layout.fragment_search, container, false);
+        final TextView textView = root.findViewById(R.id.text_search);
         homeViewModel.getText().observe(this, new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
-                textView.setText(s);
+                textView.setText("Search");
             }
         });
         return root;
