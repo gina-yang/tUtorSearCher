60182477431ec4f648e1d019686395fb04f8dd47
Lysosome <benjamjm@usc.edu>
2019-11-13 15:03:45 -0800

    Added first few Espresso test functions. Also bugfixed Search function so that it has a properly formatted dt_tm string (with space)

---
+++ app/src/androidTest/java/com/example/tutorsearcher/EspressoTest.java
@@ -0,0 +1,64 @@
+package com.example.tutorsearcher;
+
+import android.util.Log;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import androidx.test.rule.ActivityTestRule;
+import androidx.test.runner.AndroidJUnit4;
+
+import com.example.tutorsearcher.ui.login.LoginActivity;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
+@RunWith(AndroidJUnit4.class)
+public class EspressoTest {
+
+    private String stringToBetyped;
+
+    // Need this to define the activity we're going to pull elements from
+    @Rule
+    public ActivityTestRule<LoginActivity> activityRule
+            = new ActivityTestRule<>(LoginActivity.class);
+
+    @Before
+    public void initValidString() {
+        // Specify a valid string.
+        stringToBetyped = "Espresso";
+    }
+
+    @Test
+    public void testThatShouldPass() {
+        onView(withId(R.id.username))
+                .perform(typeText(stringToBetyped), closeSoftKeyboard());
+        // onView(withId(R.id.changeTextBt)).perform(click());
+
+        // Check that the text was changed.
+        onView(withId(R.id.username))
+                .check(matches(withText(stringToBetyped)));
+
+        Log.d("test", "The Espresso Pass test has run!");
+    }
+
+    @Test
+    public void testThatShouldFail() {
+        // Type text.
+        onView(withId(R.id.username))
+                .perform(typeText(stringToBetyped), closeSoftKeyboard());
+
+        // Check if the text happens to match string "Wahoo!".
+        onView(withId(R.id.username))
+                .check(matches(withText("Wahoo!")));
+
+        Log.d("test", "The Espresso Fail test has run!");
+    }
+}
---
+++ app/src/androidTest/java/com/example/tutorsearcher/TuteeTestSuite.java
@@ -0,0 +1,54 @@
+package com.example.tutorsearcher;
+
+// Test Suite for Tutee functionality
+// For now, just making sure that tutees can view profile info of accepted tutors
+// Later functionality: tutees should be able to leave and view ratings for tutors
+
+import android.util.Log;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import androidx.test.rule.ActivityTestRule;
+import androidx.test.runner.AndroidJUnit4;
+
+import com.example.tutorsearcher.activity.MainActivity;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
+@RunWith(AndroidJUnit4.class)
+public class TuteeTestSuite {
+    private String stringToBetyped;
+
+    // Need this to define the activity we're going to pull elements from
+    @Rule
+    public ActivityTestRule<MainActivity> activityRule
+            = new ActivityTestRule<>(MainActivity.class);
+
+    @Before
+    public void initValidString() {
+        // Specify a valid string.
+        stringToBetyped = "Espresso";
+    }
+
+    @Test
+    public void testThatShouldPass() {
+        onView(withId(R.id.username))
+                .perform(typeText(stringToBetyped), closeSoftKeyboard());
+        // onView(withId(R.id.changeTextBt)).perform(click());
+
+        // Check that the text was changed.
+        onView(withId(R.id.username))
+                .check(matches(withText(stringToBetyped)));
+
+        Log.d("test", "The Espresso Pass test has run!");
+    }
+}
