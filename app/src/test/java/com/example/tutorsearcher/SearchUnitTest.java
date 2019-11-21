f639298fe492d3ca81841e72069f5a2bf1c5ab29
Gina Yang <ginayang@usc.edu>
2019-11-20 20:36:25 -0800

    Revert "i luv whitebox"
    This reverts commit 7209dc9952f90504b10acfc17d929bd22efc96fd.

---
+++ app/src/test/java/com/example/tutorsearcher/SearchUnitTest.java
@@ -0,0 +1,22 @@
+package com.example.tutorsearcher;
+
+import android.util.Log;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import static org.junit.Assert.*;
+
+/**
+ * Tests if searching for something in the data base works as it should
+ */
+public class SearchUnitTest
+{
+    @Test
+    public void searchDBAccessorTest()
+    {
+        
+    }
+}
