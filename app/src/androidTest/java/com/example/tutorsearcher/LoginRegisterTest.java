dd81eef0e0d047c1e68167f85b51ced97fc95020
Gina Yang <ginayang@usc.edu>
2019-11-20 19:44:40 -0800

    Sample dbaccessor test in src/androidTest

---
+++ app/src/androidTest/java/com/example/tutorsearcher/LoginRegisterTest.java
@@ -0,0 +1,58 @@
+package com.example.tutorsearcher;
+
+import android.util.Log;
+
+import com.example.tutorsearcher.db.DBAccessor;
+import com.example.tutorsearcher.db.isNewUserCommandWrapper;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import static org.mockito.Mockito.*;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class LoginRegisterTest {
+    DBAccessor dba;
+    @Before
+    public void setup(){
+        dba = new DBAccessor();
+
+        dba.addNewUser("utesttutee@usc.edu", "password", "Tutee");
+        dba.addNewUser("utesttutor@usc.edu", "password", "Tutor");
+        Log.d("hello","hello");
+    }
+
+    @Test
+    public void testIsNewUser(){
+        class userExists extends isNewUserCommandWrapper {
+            public void execute(boolean isNew){
+                assertFalse(isNew);
+            }
+        }
+        class wrongRole extends isNewUserCommandWrapper {
+            public void execute(boolean isNew){
+                assertTrue(isNew);
+            }
+        }
+        class doesntExist extends isNewUserCommandWrapper {
+            public void execute(boolean isNew){
+                assertTrue(isNew);
+            }
+        }
+        System.out.println("testing123");
+        dba.isNewUser("utesttutee@usc.edu", "tutee", new userExists());
+        dba.isNewUser("utesttutee@usc.edu", "tutor", new wrongRole());
+        dba.isNewUser("utesttutor@usc.edu", "tutee", new wrongRole());
+        dba.isNewUser("blahblahblah@usc.edu", "tutee", new doesntExist());
+    }
+
+    @Ignore
+    public void testValidateUser(){
+
+    }
+}
